26bec0f93d000e767c2c2e6e24c96ff50c442932
vovan <shvvs@ukr.net>
2020-11-18 12:29:19 +0200

    finish t-statement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {InterpolationComponent} from './interpolation/interpolation.component'
+import {InterpolationComponent} from './interpolation/interpolation.component';
+import {TemplateStatementComponent} from './template-statement/template-statement.component'
 
 const routes: Routes = [
    { path: 'interpolation', component: InterpolationComponent },
+   { path: 'template-statement', component: TemplateStatementComponent },
 ];
 
 @NgModule({
